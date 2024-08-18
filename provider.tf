650c27890142e0535809322da38916b18b216b5c
smahesh97 <smahesh.97@gmail.com>
2024-08-18 17:14:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,12 @@
-# provider.tf
-
-# Specify the provider and access details
-provider "aws" {
-  profile                 = "default"
-  region                  = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"     
+}

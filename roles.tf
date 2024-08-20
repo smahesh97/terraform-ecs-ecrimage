f518445015c32503049e899201627c3a318088af
smahesh97 <smahesh.97@gmail.com>
2024-08-20 09:15:28 +0530

    Update roles1.tf

--- roles.tf
+++ roles.tf
@@ -1,18 +1,15 @@
 # ECS task execution role data
 data "aws_iam_policy_document" "ecs_task_execution_role" {
-  version = "2012-10-17"
-  statement {
-    sid = ""
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
 }
-
 # ECS task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = var.ecs_task_execution_role_name
